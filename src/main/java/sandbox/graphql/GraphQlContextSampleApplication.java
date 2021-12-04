ceb650078978570540c7235c0eed7348987d4f99
Rossen Stoyanchev <rstoyanchev@vmware.com>
2021-12-04 06:01:54 +0000

    Start

---
+++ src/main/java/sandbox/graphql/GraphQlContextSampleApplication.java
@@ -0,0 +1,13 @@
+package sandbox.graphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphQlContextSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphQlContextSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sandbox/graphql/GraphQlContextSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package sandbox.graphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphQlContextSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
